f54d74c7bda48bc517044a7c7a52a4ac154df850
mf15726 <mark1.fitzsimmons@live.com>
2024-09-04 22:36:46 +0100

    added first endpoint

--- go-ball-server/main.go
+++ go-ball-server/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"go.uber.org/dig"
-
 	"github.com/mf15726/go-ball/player"
+
+	"net/http"
 )
 
 func main() {
+	createContainers()
+	
+	mux := http.NewServeMux()
+
+	// Register the routes and handlers
+	mux.Handle("/", &homeHandler{})
+
+	// Run the server
+	http.ListenAndServe(":8080", mux)
+}
+
+func createContainers() {
 	container := dig.New()
 	container.Provide(player.PlayerRepositoryInMemoryImpl)
 	container.Provide(player.PlayerServiceImpl)
 }
+
+func 
